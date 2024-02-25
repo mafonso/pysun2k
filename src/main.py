f9dc6af6154c9807ab69607c1c30291eab6290a8
Miguel Afonso <miguel.afonso@mercedes-benz.io>
2024-02-25 19:58:31 +0000

    Don't keep connection to inverter

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ import signal
 import sys
 import threading
 import time
-import logging
 from threading import Event
 
 from flask import Flask, jsonify
@@ -13,71 +12,56 @@ from waitress import serve
 
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
-from influxdb_client import Point
-
-
-
-
-
 
 app = Flask(__name__)
 # Global dictionary to store status information
 inverter_data = dict()
-def update_data_thread(event: Event) -> None:
-    while not event.is_set():
+
+
+def update_data_thread(thread_event: Event) -> None:
+    while not thread_event.is_set():
         # Update the global dictionary with the current time
         inverter_data["last_updated"] = time.strftime("%Y-%m-%d %H:%M:%S")
         inverter.connect()
+
         if inverter.isConnected():
-            sn = inverter.read_raw_value(registers.InverterEquipmentRegister.SN)
-            model = inverter.read_raw_value(registers.InverterEquipmentRegister.Model)
-            number_of_strings = inverter.read_raw_value(registers.InverterEquipmentRegister.NumberOfPVStrings)
-            number_of_mppt = inverter.read_raw_value(registers.InverterEquipmentRegister.NumberOfMPPTrackers)
-            systemtime = inverter.read_raw_value(registers.InverterEquipmentRegister.SystemTime)
 
             sun2000 = dict()
+            sun2000['internal_temperature'] = inverter.read_raw_value(registers.InverterEquipmentRegister.InternalTemperature) / 10
+            sun2000['model'] = inverter.read_raw_value(registers.InverterEquipmentRegister.Model)
+            sun2000['number_of_mppt'] = inverter.read_raw_value(registers.InverterEquipmentRegister.NumberOfMPPTrackers)
+            sun2000['number_of_strings'] = inverter.read_raw_value(registers.InverterEquipmentRegister.NumberOfPVStrings)
+            sun2000['sn'] = inverter.read_raw_value(registers.InverterEquipmentRegister.SN)
+            sun2000['system_time'] = inverter.read_raw_value(registers.InverterEquipmentRegister.SystemTime)
+            sun2000['total_input_power'] = inverter.read_raw_value(registers.InverterEquipmentRegister.InputPower)
 
-            sun2000['sn'] = sn
-            sun2000['model'] = model
-            sun2000['number_of_strings'] = number_of_strings
-            sun2000['number_of_mppt'] = number_of_mppt
-            sun2000['system_time'] = systemtime
-
-
-            # PV0
+            # Todo: Interate PV Strings and MPPT Trackers
+            # PV1
             pv1_voltage = inverter.read_raw_value(registers.InverterEquipmentRegister.PV1Voltage)
             pv1_current = inverter.read_raw_value(registers.InverterEquipmentRegister.PV1Current)
-            pv0 = dict()
-            pv0['voltage'] = pv1_voltage / 10
-            pv0['current'] = pv1_current / 100
+            pv1 = dict()
+            pv1['voltage'] = pv1_voltage / 10
+            pv1['current'] = pv1_current / 100
 
-            # Current
+            # PV2
             pv2_voltage = inverter.read_raw_value(registers.InverterEquipmentRegister.PV2Voltage)
             pv2_current = inverter.read_raw_value(registers.InverterEquipmentRegister.PV2Current)
-            pv1 = dict()
-            pv1['voltage'] = pv2_voltage / 10
-            pv1['current'] = pv2_current / 100
+            pv2 = dict()
+            pv2['voltage'] = pv2_voltage / 10
+            pv2['current'] = pv2_current / 100
 
             pv_data = dict()
-            pv_data['0'] = pv0
-            pv_data['1'] = pv1
-
-            input_power = inverter.read_raw_value(registers.InverterEquipmentRegister.InputPower)
-            internal_temperature = inverter.read_raw_value(registers.InverterEquipmentRegister.InternalTemperature) / 10
-
+            pv_data['0'] = pv1
+            pv_data['1'] = pv2
             sun2000['pv'] = pv_data
-            sun2000['total_input_power'] = input_power
-            sun2000['internal_temperature'] = internal_temperature
 
             inverter_dict = dict()
             inverter_dict['0'] = sun2000
-
-
             inverter_data['inverters'] = inverter_dict
 
             # Meter
-            hasMeter = inverter.read_raw_value(registers.MeterEquipmentRegister.MeterStatus)
-            if hasMeter:
+            has_meter = inverter.read_raw_value(registers.MeterEquipmentRegister.MeterStatus)
+            if has_meter:
                 meter = dict()
                 meter['type'] = inverter.read_raw_value(registers.MeterEquipmentRegister.MeterType)
                 meter['active_power'] = inverter.read_raw_value(registers.MeterEquipmentRegister.ActivePower)
@@ -87,37 +71,40 @@ def update_data_thread(event: Event) -> None:
 
                 phase_a = dict()
                 phase_a['voltage'] = inverter.read_raw_value(registers.MeterEquipmentRegister.APhaseVoltage) / 10
-                phase_a['current'] = inverter.read_raw_value(registers.MeterEquipmentRegister.APhaseCurrent) /100
+                phase_a['current'] = inverter.read_raw_value(registers.MeterEquipmentRegister.APhaseCurrent) / 100
                 meter['phase_a'] = phase_a
 
                 if meter['type'] == 1:
                     phase_b = dict()
                     phase_b['voltage'] = inverter.read_raw_value(registers.MeterEquipmentRegister.BPhaseVoltage) / 10
-                    phase_b['current'] = inverter.read_raw_value(registers.MeterEquipmentRegister.BPhaseCurrent) /100
+                    phase_b['current'] = inverter.read_raw_value(registers.MeterEquipmentRegister.BPhaseCurrent) / 100
                     meter['phase_b'] = phase_b
 
                     phase_c = dict()
                     phase_c['voltage'] = inverter.read_raw_value(registers.MeterEquipmentRegister.CPhaseVoltage) / 10
-                    phase_c['current'] = inverter.read_raw_value(registers.MeterEquipmentRegister.CPhaseCurrent) /100
+                    phase_c['current'] = inverter.read_raw_value(registers.MeterEquipmentRegister.CPhaseCurrent) / 100
                     meter['phase_c'] = phase_c
 
                 inverter_data['meter'] = meter
 
+                inverter.disconnect()
+
+                # Todo: Move to separate function
                 write_api.write(bucket, org, [
                     {
                         "measurement": "inverter",
-                        "timestamp": systemtime,
+                        "timestamp": sun2000['system_time'],
                         "tags": {
-                            "serial_number": sn,
+                            "serial_number": sun2000['sn'],
                             "unit": inverter_unit
                         },
                         "fields": {
-                            "pv0_voltage": pv0['voltage'],
-                            "pv0_current": pv0['current'],
-                            "pv1_voltage": pv1['voltage'],
-                            "pv1_current": pv1['current'],
-                            "input_power": input_power,
-                            "internal_temperature": internal_temperature
+                            "pv0_voltage": sun2000['pv']['0']['voltage'],
+                            "pv0_current": sun2000['pv']['0']['current'],
+                            "pv1_voltage": sun2000['pv']['1']['voltage'],
+                            "pv1_current": sun2000['pv']['1']['current'],
+                            "input_power": sun2000['total_input_power'],
+                            "internal_temperature": sun2000['internal_temperature']
                         }
                     }
                 ])
@@ -125,8 +112,9 @@ def update_data_thread(event: Event) -> None:
                 write_api.write(bucket, org, [
                     {
                         "measurement": "meter",
+                        "timestamp": sun2000['system_time'],
                         "tags": {
-                            "serial_number": sn,
+                            "serial_number": sun2000['sn'],
                             "unit": inverter_unit
                         },
                         "fields": {
@@ -139,11 +127,6 @@ def update_data_thread(event: Event) -> None:
                         }
                     }
                 ])
-
-        else:
-            inverter_data["last_updated"] = time.strftime("%Y-%m-%d %H:%M:%S")
-            inverter_data['status'] = "Not connected"
-            inverter.connect()
         time.sleep(5)
 
 
@@ -158,9 +141,11 @@ def status():
     return jsonify(inverter_data)
 
 
-def signal_handler(signal, frame):
+def signal_handler(sig, frame):
+    if sig == signal.SIGINT:
+        event.set()
+        inverter.disconnect()
     sys.exit(0)
-
 
 
 if __name__ == "__main__":
@@ -184,9 +169,7 @@ if __name__ == "__main__":
     client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
     write_api = client.write_api(write_options=SYNCHRONOUS)
 
-
     inverter = inverter.Sun2000(host=inverter_host, port=inverter_port, unit=inverter_unit)
-
 
     # Create and start the update status thread
     event = Event()
